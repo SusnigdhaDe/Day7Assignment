8ed0d4e5d91a5a8687bc7097cb765a155fed3b35
Susnigdha De <susnigdha.de@gmail.com>
2020-08-31 02:08:19 +0530

    Take a range from 0 – 100, find the digits that are repeated twice like 33, 77, etc and store them in an array

--- repeat_digits.sh
+++ repeat_digits.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-#Take a range from 0 – 100, find the digits that are repeated twice 
+#Take a range from 0 – 100, find the digits that are repeated twice
 #like 33, 77, etc and store them in an array
 
 
